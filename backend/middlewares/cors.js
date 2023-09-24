a52cbfd5ada8df8b2ef1c2ef39e20a489cd89b58
Evgenii Rubin <kubikus2009@yandex.ru>
2023-09-24 23:59:06 +0300

    feat: еще коммит

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,17 +9,17 @@ module.exports = (req, res, next) => {
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
     // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
-  }
-
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
   }
 
   return next();
