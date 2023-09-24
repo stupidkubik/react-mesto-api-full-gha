8eea1aa7832af49e50dc7039cfbb5f4d0daedb1b
Evgenii Rubin <kubikus2009@yandex.ru>
2023-09-24 18:35:03 +0300

    feat: изменить бэк

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const router = require('./routes');
 
 dotenv.config();
 
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
