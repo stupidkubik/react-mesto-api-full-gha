2ca70f44988c01f4cee844bf1a3035640c8a4af6
Evgenii Rubin <kubikus2009@yandex.ru>
2023-09-24 23:49:28 +0300

    feat: еще коммит

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const rateLimit = require('express-rate-limit');
 // const cors = require('cors');
 
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 const error = require('./middlewares/error');
 const router = require('./routes');
@@ -14,6 +15,11 @@ const router = require('./routes');
 dotenv.config();
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // за 15 минут
+  max: 100, // можно совершить максимум 100 запросов с одного IP
+});
 
 mongoose
   .connect(DB_URL, {
@@ -25,26 +31,23 @@ const app = express();
 // app.use(cors());
 app.use(cors);
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 100, // можно совершить максимум 100 запросов с одного IP
-});
-
-app.use(limiter);
-
 app.disable('x-powered-by');
 app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+app.use(limiter);
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
 app.use(router);
+
+app.use(errorLogger);
 app.use(errors());
 app.use(error);
 
