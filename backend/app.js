5994d96eb0e89ce6b857c5da8116dab5012c486b
Evgenii Rubin <kubikus2009@yandex.ru>
2023-10-05 00:50:53 +0300

    feat: еще коммит

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ mongoose
   });
 
 const corsOptions = {
-  origin: 'https://stupid.kubik.nomoredomainsrocks.ru',
+  origin: 'http://localhost:3001',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
 };
